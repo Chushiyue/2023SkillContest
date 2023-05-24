87a63fec7adf38edf15daf4bb77c901de7106483
赵宁 <lzzhaoning@163.com>
2023-05-24 16:04:27 +0800

    20230524

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,40 +1,22 @@
 <template>
-  <div>
-  <div>Bot昵称：{{ bot_name }}</div>
-  <div>Bot战力：{{ bot_rating }}</div>
-  </div>
-  <router-view/>
+    <NavBar/>
+    <router-view/>
 </template>
 
 <style>
 body {
-    background-image: url("@/assets/OrteliusWorldMap1570.jpg");
-    background-size: cover;
+    //background-image: url("@/assets/OrteliusWorldMap1570.jpg");
+    //background-size: cover;
 }
 </style>
 <script>
-import $ from 'jquery';
-import { ref } from 'vue';
+import NavBar from '@/components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
 export default {
-    name: "App",
-    setup: () => {
-        let bot_name = ref("");
-        let bot_rating = ref("");
-
-        $.ajax({
-            url: "http://127.0.0.1:3000/pk/getbotinfo/",
-            type: "get",
-            success: resp => {
-                console.log(resp);
-                bot_name.value = resp.name;
-                bot_rating.value = resp.rating;
-            }
-        });
-
-        return {
-            bot_name,
-            bot_rating
-        }
+    components: {
+        NavBar
     }
 }
 </script>
---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,19 @@
+<script>
+import ContentField from '../../../components/ContentField.vue'
+
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<template>
+    <ContentField>
+        我的bot
+    </ContentField>
+</template>
+
+<style scoped>
+
+</style>
